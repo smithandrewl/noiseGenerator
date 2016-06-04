f9b6427e7a674fcdbdc0f3dce58fd1f2ea8bb166
Andrew Smith <smith.andrew.l@gmail.com>
2016-06-03 20:56:39 -0500

    Removed OpenMP shared clause

--- noise_generator.cpp
+++ noise_generator.cpp
@@ -136,7 +136,7 @@ void NoiseGenerator::generate() {
 
   randomPoints(numberOfPoints);
 
-  #pragma omp parallel private (current, closest, distance, color, base) shared(pixels)
+  #pragma omp parallel private(current, closest, distance, color, base) 
   #pragma omp for
   for(int x = 0; x < WIDTH; x++) {
     for(int y = 0; y < HEIGHT; y++) {
